3508270a1c0a3501ccd6a1e36182229d235a174e
Angelos Staboulis <angelosstaboulis@gmail.com>
2024-04-17 23:16:42 +0300

    Commit minor improvements

--- CalculateDistance/CalculateDistanceApp.swift
+++ CalculateDistance/CalculateDistanceApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CalculateDistanceApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView(destinationCoordinates: .init(), currentCoordinates: .init(), destination: "", current: "", distance: "")
+            ContentView(destinationCoordinates: .init(), currentCoordinates: .init(), destination: "", current: "", distance: "", realDistance: "")
         }
     }
 }
--- CalculateDistance/ContentView.swift
+++ CalculateDistance/ContentView.swift
@@ -13,6 +13,8 @@ struct ContentView: View {
     @State var destination:String
     @State var current:String
     @State var distance:String
+    @State var realDistance:String
+
     var body: some View {
         NavigationStack {
             VStack{
@@ -26,14 +28,19 @@ struct ContentView: View {
                 }
                 VStack{
                     Text("Distance=\(distance)").frame(width:390,height:45,alignment: .leading)
+                    Text("Real Distance=\(realDistance)").frame(width:390,height:45,alignment: .leading)
                 }
                 HStack{
                     VStack{
                         Button(action: {
                             Task{
-                                currentCoordinates = await getCoordinates(destinationValue: current)
-                                destinationCoordinates = await getCoordinates(destinationValue: destination)
-                                distance = String(format:"%.2fkm",getDistance())
+                                currentCoordinates = await Helper.shared.getCoordinates(destinationValue: current)
+                                
+                                destinationCoordinates = await Helper.shared.getCoordinates(destinationValue: destination)
+                                
+                                distance = String(format:"%.2fkm",Helper.shared.getDistance(currentCoordinates: currentCoordinates, destinationCoordinates: destinationCoordinates))
+                                
+                                realDistance = await Helper.shared.getRealDistance(currentCoordinates: currentCoordinates, destinationCoordinates: destinationCoordinates)
                             }
                         }, label: {
                             Text("Go to")
@@ -50,32 +57,10 @@ struct ContentView: View {
             .navigationBarTitleDisplayMode(.inline)
         }
     }
-    func getCoordinates(destinationValue:String) async -> CLLocationCoordinate2D{
-        return await withCheckedContinuation{contiuation in
-            let geocoder = CLGeocoder()
-            geocoder.geocodeAddressString(destinationValue) { placermark, error in
-                guard let destination = placermark else{
-                    return
-                }
-                guard let findDestination = destination.last else{
-                    return
-                }
-                guard let findCordinate = findDestination.location else{
-                    return
-                }
-                contiuation.resume(returning: findCordinate.coordinate)
-            }
-        }
-       
-    }
-    func getDistance()->CLLocationDistance{
-        let currentLocation = CLLocation(latitude:currentCoordinates.latitude , longitude: currentCoordinates.longitude)
-        let destinationLocation = CLLocation(latitude:destinationCoordinates.latitude , longitude: destinationCoordinates.longitude)
-        let distance = destinationLocation.distance(from: currentLocation)
-        return distance / 1000
-    }
+    
+    
 }
 
 #Preview {
-    ContentView(destinationCoordinates: .init(), currentCoordinates:.init(), destination: "", current: "", distance: "")
+    ContentView(destinationCoordinates: .init(), currentCoordinates:.init(), destination: "", current: "", distance: "", realDistance: "")
 }
